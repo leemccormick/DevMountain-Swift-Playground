530de98593f0b832363a5e07ec49c586540f3a7c
Lee McCormick <57606580+Tittaporn@users.noreply.github.com>
2021-02-03 00:09:08 -0600

    Dictionary

---
+++ dictionary.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+// Week 5 Day2 : Dictionary by Bryan.
+
+var dictionary: [String: String] = [:]
+var array: [String] = []
+
+//key-value
+dictionary["banana"] = "fruit"
+dictionary["apple"] = "fruit"
+dictionary["carrot"] = "vegetable"
+
+print(dictionary)
+
+
+//indexes
+array.append("banana")
+array.append("apple")
+
+print(array)
+
+
+
+var intDict: [Int : String] = [:]
+intDict[1] = "one"
+intDict[2] = "two"
+print(intDict)
+
+
